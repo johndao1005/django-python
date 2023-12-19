d64867ebdc2d3f7b2cabc6cdcb2999342cb2d1d4
John Dao <john.d1729@gmail.com>
2023-12-19 19:47:34 +1300

    change front end to react

--- finance_app-frontend/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './views/index' // Import the router configuration
-
-
-createApp(App).use(router).mount('#app')
-//  createApp(App).mount('#app')
